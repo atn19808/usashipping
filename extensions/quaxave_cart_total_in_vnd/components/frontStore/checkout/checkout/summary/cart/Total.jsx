73c2af37cc17bc465b31b82661121cb96638919d
vitran96 <38664902+vitran96@users.noreply.github.com>
2025-09-26 21:04:49 +0700

    #61 - feat: FE convert the pricing

--- extensions/quaxave_cart_total_in_vnd/components/frontStore/checkout/checkout/summary/cart/Total.jsx
+++ extensions/quaxave_cart_total_in_vnd/components/frontStore/checkout/checkout/summary/cart/Total.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { _ } from '@evershop/evershop/src/lib/locale/translate';
+import Spinner from '@components/common/Spinner';
+import { useQuery } from 'urql';
 import './Total.scss';
+
+// TODO: currency pair should come from config
+const QUERY = `
+  query Query($source: String, $target: String) {
+    fxRate(source: $source, target: $target) {
+      rate
+    }
+  }
+`;
 
 export function Total(props) {
   const { total, totalTaxAmount, priceIncludingTax } = props;
 
-  // TODO: remove
-  console.log(props);
-
   const totalText = total.text;
   const totalTaxText = totalTaxAmount.text;
+
+  const [result] = useQuery({
+    query: QUERY,
+    variables: {
+      source: "usd",
+      target: "vnd"
+    }
+  });
+  const { data, fetching, error: queryError } = result;
+
+  let vndText = '⚠️';
+  if (queryError) {
+    console.error(queryError);
+  } else if (!fetching && data !== null && data.fxRate !== null) {
+    const rate = data.fxRate.rate;
+    const vndValue = total.value * rate;
+    // TODO: check what browser compatile with API below
+    vndText = Intl.NumberFormat(
+      'vn-VN',
+      {
+        style: 'currency',
+        currency: 'VND',
+      }
+    ).format(vndValue);
+  }
+
   return (
     <div className="summary-row grand-total flex justify-between">
       {(priceIncludingTax && (
@@ -38,7 +72,7 @@ export function Total(props) {
       <div>
         <div>
           <div className="grand-total-value">{totalText}</div>
-          <div>{totalText}</div>
+          {(fetching && <Spinner width={25} height={25} />) || <div>{vndText}</div>}
         </div>
       </div>
     </div>
@@ -54,7 +88,10 @@ Total.propTypes = {
     value: PropTypes.number,
     text: PropTypes.string
   }).isRequired,
-  priceIncludingTax: PropTypes.bool
+  priceIncludingTax: PropTypes.bool,
+  fxRate: PropTypes.shape({
+    rate: PropTypes.number.isRequired
+  })
 };
 
 Total.defaultProps = {
