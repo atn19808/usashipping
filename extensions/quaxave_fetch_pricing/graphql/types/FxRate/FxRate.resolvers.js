a78c8c795972fba99191b79291fc1c3f975424cd
vitran96 <38664902+vitran96@users.noreply.github.com>
2025-09-26 09:46:52 +0700

    #51 - feat: fx_rate graphql place holder

---
+++ extensions/quaxave_fetch_pricing/graphql/types/FxRate/FxRate.resolvers.js
@@ -0,0 +1,12 @@
+module.exports = {
+    Query: {
+        fxRate: async (_, { filters = [] }) => {
+            // TODO:
+            return null;
+        },
+        fxRates: async (_, { filters = [] }) => {
+            // TODO:
+            return null;
+        }
+    }
+};
